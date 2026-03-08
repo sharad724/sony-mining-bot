446439e826d691a5e41b7537993fb669ed713995
sharad724 <80893710+sharad724@users.noreply.github.com>
2026-03-09 00:07:53 +0530

    bot/config.py

---
+++ bot/config.py
@@ -0,0 +1,33 @@
+"""
+Sony Mining Bot Configuration - SON Token
+"""
+
+# Token Settings
+TOKEN_NAME = "SON"
+APP_NAME = "Sony mining"
+TOKEN_SYMBOL = "SON"
+
+# Mining Settings
+MINING_RATE = 1.0  # SON per hour
+MAX_MINING_HOURS = 3.59
+WELCOME_BONUS = 10
+
+# Referral Settings
+REFERRAL_BONUS = 10  # SON per referral
+REFERRAL_PERCENTAGE = 10
+
+# Withdrawal Settings
+MIN_WITHDRAWAL = 1000
+WITHDRAWAL_FEE = 5
+WITHDRAWAL_COOLDOWN = 48
+
+# IMPORTANT - YAHAN APNA TELEGRAM ID DALO!
+# @userinfobot se apna ID lo
+ADMIN_IDS = [123456789]  # Isko change karo apne ID se!
+
+# Bot Settings
+BOT_USERNAME = "SON_Mining_Bot"
+SUPPORT_GROUP = "@SON_Support"
+
+# Firebase URL
+FIREBASE_DATABASE_URL = "https://sony-mining-default-rtdb.firebaseio.com/"
